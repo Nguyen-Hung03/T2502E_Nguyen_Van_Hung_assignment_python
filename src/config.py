1436be18d79e8612d5854ebb64a1cb9127c6413b
Nguyen-Hung03 <hungnguyen05112003@gmail.com>
2026-05-22 22:59:11 +0700

    update

--- src/config.py
+++ src/config.py
@@ -10,9 +10,9 @@ load_dotenv()
 @dataclass
 class Settings:
     db_host: str = os.getenv("DB_HOST", "localhost")
-    db_port: int = int(os.getenv("DB_PORT", "3306"))
+    db_port: int = int(os.getenv("DB_PORT", "3307"))
     db_user: str = os.getenv("DB_USER", "root")
-    db_password: str = os.getenv("DB_PASSWORD", "")
+    db_password: str = os.getenv("DB_PASSWORD", "123456")
     db_name: str = os.getenv("DB_NAME", "news_management")
     link_job_time: str = os.getenv("LINK_JOB_TIME", "08:00")
     detail_job_interval_minutes: int = int(os.getenv("DETAIL_JOB_INTERVAL_MINUTES", "30"))
